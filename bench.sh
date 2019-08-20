dbf834306286f2e2cde2596b8628559d8e11bef7
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2019-08-20 09:38:24 +0200

    extract bench into a function and allow to run single language

--- bench.sh
+++ bench.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-for dir in $(find . -type d -name '[!.]*' -maxdepth 1); do
-    cd "$dir"
-    ./build
-    cd ..
-    echo "Language $dir"
-    time ("$dir/fac" > number)
-    if ! diff -q expected number &> /dev/null; then
-        echo -e "FAIL\n"
-        exit 1
-    else
-        echo -e "PASS\n"
-    fi
-done
+function bench() {
+  DIR=$1
+  (
+    cd "$DIR" && ./build
+  )
+  echo "Language $DIR"
+  time ("$DIR/fac" >number)
+  if ! diff -q expected number &>/dev/null; then
+    echo -e "FAIL\n"
+    exit 1
+  else
+    echo -e "PASS\n"
+  fi
+}
+
+if [ -n "${1-}" ]; then
+  bench "$1"
+else
+  export -f bench
+  find . -type d -name '[!.]*' -maxdepth 1 -exec bash -c 'bench "$0"' {} \;
+fi
