4c87b1269e02b2421b94f48333844e1dede36a3c
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2019-09-23 10:30:40 +0200

    format csharp with dotnet format

--- csharp/Fac.cs
+++ csharp/Fac.cs
@@ -6,19 +6,22 @@ namespace csharp
     class Fac
     {
         static void Main(string[] args)
-        {   
+        {
             BigInteger res = new BigInteger(0);
 
-            for(int i = 1; i<=3000; i++) {
+            for (int i = 1; i <= 3000; i++)
+            {
                 res += fac(i);
             }
             Console.Write(res);
         }
 
-        static BigInteger fac(int n) {
+        static BigInteger fac(int n)
+        {
             BigInteger res = new BigInteger(1);
 
-            for(;n>1;n--) {
+            for (; n > 1; n--)
+            {
                 res *= n;
             }
 
