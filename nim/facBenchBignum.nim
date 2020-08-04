234187e49a32a3a30e88767587b2e8702a7d895f
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2020-08-04 11:02:22 +0200

    try with proc keyword

--- nim/facBenchBignum.nim
+++ nim/facBenchBignum.nim
@@ -1,11 +1,11 @@
 import bignum
 
-func fac*(n: int): Int =
+proc fac*(n: int): Int =
   result = newInt(1)
   for i in 1..n:
     discard result.mul(result, i)
 
-func facBench*(n: int): Int =
+proc facBench*(n: int): Int =
   result = newInt(0)
   for i in 1..n:
     discard result.add(result, fac(i))
