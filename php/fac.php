c1d3409b1319855a02ae16771beb416ada2b8ebe
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2020-08-12 13:25:16 +0200

    use gmp in php

--- php/fac.php
+++ php/fac.php
@@ -1,16 +1,8 @@
 #!/usr/bin/env php
 <?php
 
-function fac($x) {
-    $res = 1;
-    for(;$x > 0;$x--) {
-        $res = bcmul($res, $x);
-    }
-    return $res;
-}
-
 $res = 0;
 for($i = 1;$i <= 3000;$i++) {
-    $res = bcadd($res, fac($i)); 
+    $res = gmp_add($res, gmp_fact($i));
 }
 echo $res;
