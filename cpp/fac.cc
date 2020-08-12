94dd1ecf988896f015dbe21eceff92ee0cdc4005
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2020-08-12 15:24:47 +0200

    use builtin for cpp and simplify php

--- cpp/fac.cc
+++ cpp/fac.cc
@@ -1,21 +1,11 @@
 #include <iostream>
 #include <gmpxx.h>
 
-mpz_class fac(unsigned x) {
-	mpz_class res(1);
-
-	for (; x > 1; --x) {
-		res *= x;
-	}
-
-	return res;
-}
-
 int main(void) {
 	mpz_class sum;
 
 	for (auto x = 1; x <= 3000; x++) {
-		sum += fac(x);
+		sum += mpz_class::factorial(x);
 	}
 
 	std::cout << sum;
--- php/fac.php
+++ php/fac.php
@@ -3,6 +3,6 @@
 
 $res = 0;
 for($i = 1;$i <= 3000;$i++) {
-    $res = gmp_add($res, gmp_fact($i));
+    $res += gmp_fact($i);
 }
 echo $res;
