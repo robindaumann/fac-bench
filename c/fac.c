27eda8a69e87619e577c2f8b1f6ea2f4d427fc21
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2020-08-12 13:47:23 +0200

    use builtin fac in c

--- c/fac.c
+++ c/fac.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 #include <gmp.h>
 
-void fac(mpz_t res, unsigned x) {
-	mpz_set_ui(res, 1);
-	for (; x > 1; --x) {
-		mpz_mul_ui(res, res, x);
-	}
-}
-
 int main(void) {
-	mpz_t f, res;
-	mpz_inits(f, res, NULL);
+	mpz_t fac, res;
+	mpz_inits(fac, res, NULL); /* fac = res = 0 */
 
 	for (unsigned x = 1; x <= 3000; x++) {
-		fac(f, x);
-		mpz_add(res, res, f);
+		mpz_fac_ui(fac, x);
+		mpz_add(res, res, fac);
 	}
 
 	mpz_out_str(stdout, 10, res);
-	mpz_clears(f, res, NULL);
+	mpz_clears(fac, res, NULL);
 	return 0;
 }
